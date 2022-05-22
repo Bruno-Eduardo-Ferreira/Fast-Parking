f8a477ccc5a7f7ec68e5ec949887ab1ab402c6ee
Bruno-Eduardo-Ferreira <102911080+Bruno-Eduardo-Ferreira@users.noreply.github.com>
2022-05-22 17:44:10 -0300

    fix-scroll-login

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -35,91 +35,96 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Center(
-            child: Padding(
-              padding: EdgeInsets.only(top: 55.0),
-              child: Text(
-                "Login",
-                style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Column(
+            children: [
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 55.0),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 18.0),
-            child: Image.asset('assets/images/logo.png'),
-          ),
-          SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                        controller: email,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Email',
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Informe o email corretamente!';
-                          }
-                          return null;
-                        }),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      controller: senha,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Senha',
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Informe sua senha!';
-                        } else if (value.length < 6) {
-                          return 'Sua senha deve ter no mínimo 6 caracteres';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          login();
-                        }
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(Icons.check),
-                          Padding(
-                            padding: EdgeInsets.all(16),
-                            child: Text(
-                              'Logar',
-                              style: TextStyle(fontSize: 20),
+              Padding(
+                padding: const EdgeInsets.only(top: 18.0),
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: TextFormField(
+                            controller: email,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Email',
                             ),
-                          ),
-                        ],
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Informe o email corretamente!';
+                              }
+                              return null;
+                            }),
                       ),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: TextFormField(
+                          controller: senha,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Senha',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Informe sua senha!';
+                            } else if (value.length < 6) {
+                              return 'Sua senha deve ter no mínimo 6 caracteres';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              login();
+                            }
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(Icons.check),
+                              Padding(
+                                padding: EdgeInsets.all(16),
+                                child: Text(
+                                  'Logar',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          )
-        ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
